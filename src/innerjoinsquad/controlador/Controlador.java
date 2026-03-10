8ced411906188910f73b4853ff20461f138fb298
JacoboBT <233518505+jacobobt@users.noreply.github.com>
2026-03-10 12:56:34 +0100

    creo codigo clase controlador en controlador, puente entre Vista y Datos

--- src/innerjoinsquad/controlador/Controlador.java
+++ src/innerjoinsquad/controlador/Controlador.java
@@ -1,4 +1,35 @@
 package innerjoinsquad.controlador;
 
+import innerjoinsquad.modelo.Articulo;
+import innerjoinsquad.modelo.Cliente;
+import innerjoinsquad.modelo.Datos;
+import innerjoinsquad.modelo.Pedido;
+
 public class Controlador {
+
+    private Datos datos;
+
+    public Controlador() {
+        this.datos = new Datos();
+    }
+
+    public Datos getDatos() {
+        return datos;
+    }
+
+    public void setDatos(Datos datos) {
+        this.datos = datos;
+    }
+
+    public void anadirCliente(Cliente cliente) {
+        datos.anadirCliente(cliente);
+    }
+
+    public void anadirArticulo(Articulo articulo) {
+        datos.anadirArticulo(articulo);
+    }
+
+    public void anadirPedido(Pedido pedido) {
+        datos.anadirPedido(pedido);
+    }
 }
--- src/innerjoinsquad/modelo/Datos.java
+++ src/innerjoinsquad/modelo/Datos.java
@@ -1,4 +1,37 @@
 package innerjoinsquad.modelo;
 
 public class Datos {
+
+    private Tienda tienda;
+
+    public Datos() {
+        this.tienda = new Tienda("Online Store");
+    }
+
+    public Tienda getTienda() {
+        return tienda;
+    }
+
+    public void setTienda(Tienda tienda) {
+        this.tienda = tienda;
+    }
+
+    public void anadirCliente(Cliente cliente) {
+        tienda.anadirCliente(cliente);
+    }
+
+    public void anadirArticulo(Articulo articulo) {
+        tienda.anadirArticulo(articulo);
+    }
+
+    public void anadirPedido(Pedido pedido) {
+        tienda.anadirPedido(pedido);
+    }
+
+    @Override
+    public String toString() {
+        return "Datos{" +
+                "tienda=" + tienda +
+                '}';
+    }
 }
