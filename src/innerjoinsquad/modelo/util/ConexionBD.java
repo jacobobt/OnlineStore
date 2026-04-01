78354b288d25d586b0d69d348f305aad2a02d9ae
maria-ricart <mricartmarti@uoc.edu>
2026-04-01 22:28:04 +0200

    Update ConexionBD.java

--- src/innerjoinsquad/modelo/util/ConexionBD.java
+++ src/innerjoinsquad/modelo/util/ConexionBD.java
@@ -8,7 +8,7 @@ public class ConexionBD {
 
     private static final String URL = "jdbc:mysql://localhost:3306/online_store";
     private static final String USUARIO = "root";
-    private static final String PASSWORD = "Contrasenya1433.";
+    private static final String PASSWORD = "";
 
     public static Connection getConexion() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, PASSWORD);
