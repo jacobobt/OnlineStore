7dd4dc4c0e1302d5dcacd95d02b959f0de20a494
cristianlv86 <cristianlv86@gmail.com>
2026-04-20 17:00:44 +0200

    Añadir anotaciones JPA a ClienteEstandar.java
    He importado dos jakarta.persistance (Entity y DiscriminatorValue). He añadido @Entity y @DiscriminatorValue("ESTANDAR") y finlamente he añadido el constructor vacio para JPA.

--- src/innerjoinsquad/modelo/ClienteEstandar.java
+++ src/innerjoinsquad/modelo/ClienteEstandar.java
@@ -1,10 +1,20 @@
 package innerjoinsquad.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.DiscriminatorValue;
+
+@Entity
+@DiscriminatorValue("ESTANDAR")
+
 public class ClienteEstandar extends Cliente {
 
     public ClienteEstandar(String nombreCliente, String domicilioCliente, String nifCliente, String emailCliente) {
         super(nombreCliente, domicilioCliente, nifCliente, emailCliente);
     }
+
+    // Constructor vació que añado porque es obligatorio para JPA
+    public ClienteEstandar() {}
+
     //lo mantiene falso, como estaba por defecto
     @Override
     public boolean esPremium() {
