178b554323ce227a98782982eb09aff798fd629c
cristianlv86 <cristianlv86@gmail.com>
2026-04-26 15:58:34 +0200

    Crear PruebaInsertarPedidoJPA
    Creación de la prueba para insertar pedido de JPA.

---
+++ src/innerjoinsquad/PruebaInsertarPedidoJPA.java
@@ -0,0 +1,43 @@
+package innerjoinsquad;
+
+import innerjoinsquad.modelo.Articulo;
+import innerjoinsquad.modelo.Cliente;
+import innerjoinsquad.modelo.ClienteEstandar;
+import innerjoinsquad.modelo.Pedido;
+import innerjoinsquad.modelo.dao.jpa.PedidoDAOJPA;
+
+import java.time.LocalDateTime;
+
+public class PruebaInsertarPedidoJPA {
+
+    public static void main(String[] args) {
+
+        Cliente cliente = new ClienteEstandar(
+                "Juan Pérez",
+                "Calle Falsa 123",
+                "12345678A",
+                "juan@email.com"
+        );
+
+        Articulo articulo = new Articulo(
+                "A002",
+                "Teclado mecánico",
+                79.99,
+                5.50,
+                30
+        );
+
+        Pedido pedido = new Pedido(
+                0,
+                cliente,
+                articulo,
+                2,
+                LocalDateTime.now()
+        );
+
+        PedidoDAOJPA pedidoDAO = new PedidoDAOJPA();
+        pedidoDAO.insertarPedido(pedido);
+
+        System.out.println("Pedido insertado correctamente en la base de datos.");
+    }
+}
